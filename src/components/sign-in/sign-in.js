4cb96b13ec66c1193553ad4ef0ab48843579953d
Vitaliy <mushpakov.vitaliy@gmail.com>
2021-07-09 20:05:32 +0300

    www

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -1,23 +1,31 @@
-import { signIn } from '../../api/api-handlers.js';
-import { setToken } from '../../shared/ls-service.js';
-import { routs } from '../../shared/constants/routs.js';
+import { signIn } from "../../api/api-handlers.js";
+import { setToken } from "../../shared/ls-service.js";
+import { routs } from "../../shared/constants/routs.js";
 
 export const signInHandler = () => {
-  const signInForm = document.querySelector('.sign-in__form');
+  const signInForm = document.querySelector(".sign-in__form");
+  const signInBtn = document.getElementById("signInBtn");
+  const passwordInput = document.getElementById("password");
+  const emailInput = document.getElementById("email")
 
-  signInForm.addEventListener('submit', event => {
+  // signInBtn.setAttribute("disabled", true);
+
+  signInForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    
-    signIn(email, password)
-    .then( response => {
+    const email = emailInput.value;
+    const password = passwordInput.value;
+
+    signIn(email, password).then((response) => {
       if (response) {
         const { idToken: token } = response.data;
         setToken(token);
         window.location.href = routs.home;
       }
-    })
-  })
+    });
+  });
+  passwordInput.oninput = () => {
+    const str = passwordInput.value;
+    console.log(str.match(/^.{6,}$/));
+  }
 };
